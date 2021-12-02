cca390f27b39ce3f318a945db7720d0ea0d5884d
Kawsar Ahmed <k.kawsar0095@gmail.com>
2021-12-02 08:50:11 +0600

    memory game finished

---
+++ src/components/SingleCard.js
@@ -0,0 +1,22 @@
+import '../css/SingleCard.css';
+
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+  return (
+    <div className="card">
+      <div className={flipped ? 'flipped' : ''}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          onClick={handleClick}
+          className="back"
+          src="/img/cover.jpg"
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
+}
